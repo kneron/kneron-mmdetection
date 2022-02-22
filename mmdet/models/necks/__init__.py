d6ab94502f92ec1f04efe922623ad978f7b51211
Yosuke Shinya <42844407+shinya7y@users.noreply.github.com>
2022-02-22 10:45:09 +0900

    [Feature] Support DyHead (#6823)
    * add DyHead
    
    * move and update DYReLU
    
    * update
    
    * replace stack with sum to reduce memory
    
    * clean and update
    
    * update to align inference accuracy (incomplete)
    
    * fix pad
    
    * update to align training accuracy and pick #6867
    
    * add README and metafile
    
    * update docs
    
    * resolve comments
    
    * revert picking 6867
    
    * update README.md
    
    * update metafile.yml
    
    * resolve comments and update urls

--- mmdet/models/necks/__init__.py
+++ mmdet/models/necks/__init__.py
@@ -4,6 +4,7 @@ from .bfp import BFP
 from .channel_mapper import ChannelMapper
 from .ct_resnet_neck import CTResNetNeck
 from .dilated_encoder import DilatedEncoder
+from .dyhead import DyHead
 from .fpg import FPG
 from .fpn import FPN
 from .fpn_carafe import FPN_CARAFE
@@ -15,11 +16,14 @@ from .rfp import RFP
 from .ssd_neck import SSDNeck
 from .yolo_neck import YOLOV3Neck
 from .yolox_pafpn import YOLOXPAFPN
-from .fcos_neck_kneron import FCOSNeckKneron
+# Kneron Modified Neck(s)
+from .fcos_neck_kneron import FCOSNeckKneron  # noqa: F401
 
 __all__ = [
     'FPN', 'BFP', 'ChannelMapper', 'HRFPN', 'NASFPN', 'FPN_CARAFE', 'PAFPN',
     'NASFCOS_FPN', 'RFP', 'YOLOV3Neck', 'FPG', 'DilatedEncoder',
-    'CTResNetNeck', 'SSDNeck', 'YOLOXPAFPN',
-    'FCOSNeckKneron'
+    'CTResNetNeck', 'SSDNeck', 'YOLOXPAFPN', 'DyHead'
 ]
+
+# Kneron Modified Neck(s)
+__all__.extend(['FCOSNeckKneron'])
