76b841b6417a9d50cb82e888a129e45280711eac
chingning.chen <chingning.chen@kneron.us>
2022-04-21 16:54:10 +0800

    style: fix single_stage.py style

--- mmdet/models/detectors/single_stage.py
+++ mmdet/models/detectors/single_stage.py
@@ -1,6 +1,5 @@
 # All modification made by Kneron Corp.: Copyright (c) 2022 Kneron Corp.
 # Copyright (c) OpenMMLab. All rights reserved.
-# from asyncio.windows_events import NULL
 import warnings
 
 import torch
@@ -31,22 +30,18 @@ class SingleStageDetector(BaseDetector):
     output features of the backbone+neck.
     """
 
-    def __init__(
-        self,
-        backbone,
-        neck=None,
-        bbox_head=None,
-        train_cfg=None,
-        test_cfg=None,
-        pretrained=None,
-        init_cfg=None,
-    ):
+    def __init__(self,
+                 backbone,
+                 neck=None,
+                 bbox_head=None,
+                 train_cfg=None,
+                 test_cfg=None,
+                 pretrained=None,
+                 init_cfg=None):
         super(SingleStageDetector, self).__init__(init_cfg)
         if pretrained:
-            warnings.warn(
-                "DeprecationWarning: pretrained is deprecated, "
-                'please use "init_cfg" instead'
-            )
+            warnings.warn('DeprecationWarning: pretrained is deprecated, '
+                          'please use "init_cfg" instead')
             backbone.pretrained = pretrained
         self.backbone = build_backbone(backbone)
         if neck is not None:
@@ -73,78 +68,84 @@ class SingleStageDetector(BaseDetector):
         outs = self.bbox_head(x)
         return outs
 
-    def forward_kneron(self, img, img_metas, rescale=False, return_loss=False, tracking=False):
+    def forward_kneron(self,
+                       img,
+                       img_metas,
+                       rescale=False,
+                       return_loss=False,
+                       tracking=False):
         if not tracking:
-            for var, name in [(img, "imgs"), (img_metas, "img_metas")]:
+            for var, name in [(img, 'imgs'), (img_metas, 'img_metas')]:
                 if not isinstance(var, list):
-                    raise TypeError(f"{name} must be a list, but got {type(var)}")
-    
+                    raise TypeError(
+                        f'{name} must be a list, but got {type(var)}')
+
             num_augs = len(img)
             if num_augs != len(img_metas):
                 raise ValueError(
-                    f"num of augmentations ({len(img)}) "
-                    f"!= num of image meta ({len(img_metas)})"
-                )
-    
+                    f'num of augmentations ({len(img)}) '
+                    f'!= num of image meta ({len(img_metas)})')
+
             # NOTE the batched image size information may be useful, e.g.
             # in DETR, this is needed for the construction of masks, which is
             # then used for the transformer_head.
             for im, img_meta in zip(img, img_metas):
                 batch_size = len(img_meta)
                 for img_id in range(batch_size):
-                    img_meta[img_id]["batch_input_shape"] = tuple(im.size()[-2:])
-    
+                    img_meta[img_id]['batch_input_shape'] = tuple(
+                        im.size()[-2:]
+                    )
+
             outs = None
-            if "proposals" in img_meta:
-                img_meta["proposals"] = img_meta["proposals"][0]
+            if 'proposals' in img_meta:
+                img_meta['proposals'] = img_meta['proposals'][0]
             img = img[0]
             img_metas = img_metas[0]
 
         results_list = None
-        if hasattr(self, "__Kn_ONNX_Sess__"):
-            tmp = getattr(self, "__Kn_ONNX_Sess__").run(
-                None, {"input": img.cpu().detach().numpy()}
+        if hasattr(self, '__Kn_ONNX_Sess__'):
+            tmp = getattr(self, '__Kn_ONNX_Sess__').run(
+                None, {'input': img.cpu().detach().numpy()}
             )
             device = torch.device(
-                "cuda:0" if torch.cuda.is_available() else "cpu"
-            )
+                'cuda:0' if torch.cuda.is_available() else 'cpu')
+
             res = []
 
             for o in tmp:
                 res.append(torch.from_numpy(o).float().to(device))
 
             assert hasattr(
-                self.bbox_head, "get_bboxes_kn"
-            ), "Error: None implemented kneron bbox_head forward type!"
+                self.bbox_head, 'get_bboxes_kn'
+            ), 'Error: None implemented kneron bbox_head forward type!'
 
             results_list = self.bbox_head.get_bboxes_kn(
                 res, img_metas=img_metas, rescale=rescale
             )
 
-        elif hasattr(self, "__Kn_PLUS_Params__"):
+        elif hasattr(self, '__Kn_PLUS_Params__'):
             tmp_img = img.cpu().detach().numpy()[0].transpose(1, 2, 0)
             tmp_img *= 256
             tmp_img += 128
             tmp_img = tmp_img.astype(np.uint8)
             img_bgr565 = cv2.cvtColor(src=tmp_img, code=cv2.COLOR_BGR2BGR565)
 
-            kp_params = getattr(self, "__Kn_PLUS_Params__")
+            kp_params = getattr(self, '__Kn_PLUS_Params__')
             try:
                 kip.generic_raw_inference_send(
-                    device_group=kp_params["device_group"],
+                    device_group=kp_params['device_group'],
                     generic_raw_image_header=kp_params[
-                        "generic_raw_image_header"
+                        'generic_raw_image_header'
                     ],
                     image=img_bgr565,
                     image_format=kp.ImageFormat.KP_IMAGE_FORMAT_RGB565,
                 )
 
                 generic_raw_res = kip.generic_raw_inference_receive(
-                    device_group=kp_params["device_group"],
+                    device_group=kp_params['device_group'],
                     generic_raw_image_header=kp_params[
-                        "generic_raw_image_header"
-                    ],
-                    model_nef_descriptor=kp_params["model_nef_descriptor"],
+                        'generic_raw_image_header'],
+                    model_nef_descriptor=kp_params['model_nef_descriptor'],
                 )
 
                 inf_node_output_list = []
@@ -162,22 +163,21 @@ class SingleStageDetector(BaseDetector):
             except Exception as e:
                 raise Exception(
                     e,
-                    "Kneron PLUS software have something wrong. "
-                    "If Kneron PLUS software haven't been installed, "
-                    "please check the document: "
-                    "http://doc.kneron.com/docs/#plus_python/. "
-                    "If you found something unexpected, "
-                    "welcome to give us some feedbacks in our forum "
-                    "https://www.kneron.com/forum to help us make it better!",
+                    'Kneron PLUS software have something wrong. '
+                    'If Kneron PLUS software haven\'t been installed, '
+                    'please check the document: '
+                    'http://doc.kneron.com/docs/#plus_python/. '
+                    'If you found something unexpected, '
+                    'welcome to give us some feedbacks in our forum '
+                    'https://www.kneron.com/forum to help us make it better!',
                 )
 
             assert hasattr(
-                self.bbox_head, "get_bboxes_kn"
-            ), "Error: None implemented kneron bbox_head forward type!"
+                self.bbox_head, 'get_bboxes_kn'
+            ), 'Error: None implemented kneron bbox_head forward type!'
 
             device = torch.device(
-                "cuda:0" if torch.cuda.is_available() else "cpu"
-            )
+                'cuda:0' if torch.cuda.is_available() else 'cpu')
 
             res = []
             for o1 in inf_node_output_list:
@@ -201,9 +201,12 @@ class SingleStageDetector(BaseDetector):
 
         return bbox_results
 
-    def forward_train(
-        self, img, img_metas, gt_bboxes, gt_labels, gt_bboxes_ignore=None
-    ):
+    def forward_train(self,
+                      img,
+                      img_metas,
+                      gt_bboxes,
+                      gt_labels,
+                      gt_bboxes_ignore=None):
         """
         Args:
             img (Tensor): Input images of shape (N, C, H, W).
@@ -224,9 +227,8 @@ class SingleStageDetector(BaseDetector):
         """
         super(SingleStageDetector, self).forward_train(img, img_metas)
         x = self.extract_feat(img)
-        losses = self.bbox_head.forward_train(
-            x, img_metas, gt_bboxes, gt_labels, gt_bboxes_ignore
-        )
+        losses = self.bbox_head.forward_train(x, img_metas, gt_bboxes,
+                                              gt_labels, gt_bboxes_ignore)
         return losses
 
     def simple_test(self, img, img_metas, rescale=False):
@@ -245,8 +247,7 @@ class SingleStageDetector(BaseDetector):
         """
         feat = self.extract_feat(img)
         results_list = self.bbox_head.simple_test(
-            feat, img_metas, rescale=rescale
-        )
+            feat, img_metas, rescale=rescale)
         bbox_results = [
             bbox2result(det_bboxes, det_labels, self.bbox_head.num_classes)
             for det_bboxes, det_labels in results_list
@@ -271,15 +272,13 @@ class SingleStageDetector(BaseDetector):
                 The outer list corresponds to each image. The inner list
                 corresponds to each class.
         """
-        assert hasattr(self.bbox_head, "aug_test"), (
-            f"{self.bbox_head.__class__.__name__}"
-            " does not support test-time augmentation"
-        )
+        assert hasattr(self.bbox_head, 'aug_test'), \
+            f'{self.bbox_head.__class__.__name__}' \
+            ' does not support test-time augmentation'
 
         feats = self.extract_feats(imgs)
         results_list = self.bbox_head.aug_test(
-            feats, img_metas, rescale=rescale
-        )
+            feats, img_metas, rescale=rescale)
         bbox_results = [
             bbox2result(det_bboxes, det_labels, self.bbox_head.num_classes)
             for det_bboxes, det_labels in results_list
@@ -303,18 +302,17 @@ class SingleStageDetector(BaseDetector):
 
         # get shape as tensor
         img_shape = torch._shape_as_tensor(img)[2:]
-        img_metas[0]["img_shape_for_onnx"] = img_shape
+        img_metas[0]['img_shape_for_onnx'] = img_shape
         # get pad input shape to support onnx dynamic shape for exporting
         # `CornerNet` and `CentripetalNet`, which 'pad_shape' is used
         # for inference
-        img_metas[0]["pad_shape_for_onnx"] = img_shape
+        img_metas[0]['pad_shape_for_onnx'] = img_shape
 
         if len(outs) == 2:
             # add dummy score_factor
             outs = (*outs, None)
         # TODO Can we change to `get_bboxes` when `onnx_export` fail
         det_bboxes, det_labels = self.bbox_head.onnx_export(
-            *outs, img_metas, with_nms=with_nms
-        )
+            *outs, img_metas, with_nms=with_nms)
 
         return det_bboxes, det_labels
